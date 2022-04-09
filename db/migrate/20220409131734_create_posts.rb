676715883c4a61fba4886b6fba26c8413e9bd08d
Afiz Savage <savageafiz9@gmail.com>
2022-04-09 13:28:21 +0000

    Added foreignkeys and indexes

--- db/migrate/20220409131734_create_posts.rb
+++ db/migrate/20220409131734_create_posts.rb
@@ -1,7 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-
+      t.references :author_id, null: false, foreign_key: { to_table: :users }, index: true
+      t.string 'title'
+      t.text 'text'
+      t.integer 'like_counter'
+      t.integer 'comments_counter'
       t.timestamps
     end
   end
--- db/migrate/20220409131811_create_comments.rb
+++ db/migrate/20220409131811_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-
+      t.references :author_id, null: false, foreign_key: { to_table: :users }, index: true
+      t.references :post_id, null: false, foreign_key: { to_table: :posts }, index: true
+      t.text 'text'
       t.timestamps
     end
   end
