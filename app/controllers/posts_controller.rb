e339e541e473eaa64b87fd74764eb6753e9f6ab2
Afiz Savage <savageafiz9@gmail.com>
2022-04-14 15:59:51 +0000

    Added content to the posts index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @user_posts = @user.posts.includes(:comments, :likes)
+  end
 
   def show; end
 end
